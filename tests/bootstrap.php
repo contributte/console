49e596ff96f28d4fd508a9bb6bc30e1144d33d36
Milan Felix Šulc <rkfelix@gmail.com>
2018-04-06 21:43:54 +0200

    Tests: simplify bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,6 @@
 <?php
 
-use Tester\Environment;
-use Tester\Helpers;
+use Ninjify\Nunjuck\Environment;
 
 if (@!include __DIR__ . '/../vendor/autoload.php') {
 	echo 'Install Nette Tester using `composer update --dev`';
@@ -9,25 +8,6 @@ if (@!include __DIR__ . '/../vendor/autoload.php') {
 }
 
 // Configure environment
-Environment::setup();
-date_default_timezone_set('Europe/Prague');
-
-// Create temporary directory
-define('TMP_DIR', __DIR__ . '/tmp');
-@mkdir(TMP_DIR, 0777, TRUE);
-define('CACHE_DIR', TMP_DIR . '/cache');
-@mkdir(CACHE_DIR, 0777, TRUE);
-define('TEMP_DIR', TMP_DIR . '/cases/' . getmypid());
-@mkdir(TEMP_DIR, 0777, TRUE);
-
-// Purge temporary directory
-Helpers::purge(TEMP_DIR);
-
-/**
- * @param Closure $function
- * @return void
- */
-function test(\Closure $function)
-{
-	$function();
-}
+Environment::setupTester();
+Environment::setupTimezone();
+Environment::setupVariables(__DIR__);
