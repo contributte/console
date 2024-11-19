aa6c722515007fed06c8dcf7e367b3068b6c87bf
Jirka Hrazdil <jiri.hrazdil@inspire.cz>
2024-11-19 19:32:25 +0100

    add test for a command with an alias and for a hidden command

---
+++ tests/Fixtures/FooHiddenCommand.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types = 1);
+
+namespace Tests\Fixtures;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(
+	name: 'app:foo',
+	description: 'Foo command',
+	hidden: true
+)]
+final class FooHiddenCommand extends Command
+{
+
+	protected function execute(InputInterface $input, OutputInterface $output): int
+	{
+		return 0;
+	}
+
+}
